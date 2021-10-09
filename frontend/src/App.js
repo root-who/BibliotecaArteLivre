b3cd71cd4c1c579d6a57e77f63b846d61466705d
root_who <joaopaulo_gonzaga@outlook.com>
2021-10-09 07:56:00 -0300

    Header da pagina foi criado

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import HeaderPag from "./componentes/header/index.jsx";
+import React from 'react';
+
+function App() {
+  return (
+
+      <HeaderPag></HeaderPag>
+
+  );
+}
+
+export default App;
